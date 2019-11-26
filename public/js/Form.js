ab86ab0543e70b2b01255091dcd14f007f49502b
yann <git config --global user.email>
2019-11-26 22:30:11 +0100

    Controll if required inputs are complete 	before sending form. 	except for checkbox : to do

--- public/js/Form.js
+++ public/js/Form.js
@@ -1,23 +1,43 @@
 class Form {
-    constructor(inputs, prefixUrl){
-        this.inputs = inputs;
+    constructor(formElt, prefixUrl){
+        this.form = formElt;
         this.url = this.setUrl(prefixUrl);
         console.log(this.url);
-        this.standards = {}; 
+        this.standards = {};
+        this.errors = [];
     }
     
     setUrl(prefixUrl){
         let names=[];
-        for (var i = 0; i < this.inputs.length; i++) {
-            names[i] = this.inputs[i].name;
+        for (var i = 0; i < this.form.elements.length - 1 ; i++) {
+            names[i] = this.form.elements[i].name;
         }
 
         names = names.toString();
         return prefixUrl + names;
     }
     
-    setStandards(reponse) {
-        Object.assign(this.standards, JSON.parse(reponse));
+    isValid() {
+        //console.log(this.form.elements);
+        //console.log(this.standards);
+
+        for (let standard in this.standards) {
+            let input = this.form.elements.namedItem(standard);
+            console.log(input);
+            if (input.value.length === 0 && this.standards[standard].required) {
+                input.classList.add('border-danger');
+                $('#' + input.id + ' + .invalid-feedback').css('display', 'block');
+                this.errors.push('champ requis');
+            }
+            //console.log(input);
+        }
+        //console.log(this.errors);
+        
+        if (this.errors.length === 0){
+            return true;
+        }
+        
+        return false;
     }
 
 }
--- public/js/main.js
+++ public/js/main.js
@@ -11,11 +11,21 @@ let prefixUrl = 'http://projet-5/form-validation/';
 let form;
 
 if ($('form.needs-validation') !== null) {
-    
-    form = new Form($("input:not(:submit)"), prefixUrl);    
-    
+        
+    form = new Form(document.querySelector('form'), prefixUrl);    
+
     ajaxGet(form.url, function (reponse){
-        form.setStandards(reponse);
+        Object.assign(form.standards, JSON.parse(reponse));
+    });
+    
+    form.form.addEventListener('submit', function (e) {
+        if (!form.isValid()){
+            console.log('formulaire recalé');
+            e.preventDefault();
+        } else {
+            console.log('formulaire envoyé');
+            e.preventDefault();
+        }
     });
 }
 
