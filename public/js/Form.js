a64d933e06eedcebb2c8ae57f02a9330cf884fb6
yann <git config --global user.email>
2019-11-27 11:03:38 +0100

    fix control for checkbox before sending

--- public/js/Form.js
+++ public/js/Form.js
@@ -23,21 +23,26 @@ class Form {
 
         for (let standard in this.standards) {
             let input = this.form.elements.namedItem(standard);
-            console.log(input);
-            if (input.value.length === 0 && this.standards[standard].required) {
-                input.classList.add('border-danger');
-                $('#' + input.id + ' + .invalid-feedback').css('display', 'block');
-                this.errors.push('champ requis');
+            if (input.type === 'checkbox' && !input.checked && this.standards[standard].required) {
+                this.showError(input);
             }
-            //console.log(input);
+            else if (input.value.length === 0 && this.standards[standard].required) {
+                this.showError(input);
+            }
         }
-        //console.log(this.errors);
+        console.log(this.errors);
         
         if (this.errors.length === 0){
             return true;
         }
         
         return false;
+    }
+    
+    showError(input) {
+        input.classList.add('border-danger');
+        $('#' + input.id + ' ~ .invalid-feedback').css('display', 'block');
+        this.errors.push('champ requis : ' + input.name);
     }
 
 }
